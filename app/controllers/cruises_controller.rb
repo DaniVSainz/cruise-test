fbfccee37380456f9c19a4fb71768c96365e0168
DaniVSainz <danivsainz@gmail.com>
2017-04-17 12:46:20 -0400

    Add normalize,httparty,started main construction on index page, add httparty get request inside index controller

--- app/controllers/cruises_controller.rb
+++ app/controllers/cruises_controller.rb
@@ -1,6 +1,8 @@
 class CruisesController < ApplicationController
 
   def index
+    @userless_request = "https://api.myjson.com/bins/2gr36"
+    @response = HTTParty.get @userless_request
   end
-  
+
 end
